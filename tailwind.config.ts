9452522f566d5504a1de5d66143db9ca84b2f207
Aditya Pratap <adityasinghpratap2733@gmail.com>
2024-04-05 19:55:53 +0530

    code reveiw

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        customFont: ['Roboto', 'sans-serif'], // Replace with your font name
+      },
     },
   },
   plugins: [],
